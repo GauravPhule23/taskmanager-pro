1bd4f560fb1703dd173763646a313f64dca1bcb7
Gaurav Phule <gaurav230605@gmail.com>
2025-12-30 16:17:05 +0530

    implemented password hashing

--- Model/user.js
+++ Model/user.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const {createHmac, randomBytes} = require('crypto')
+const {createToken} = require('../Services/token')
 
 const userModel = new mongoose.Schema({
   firstName : {type:String, required:true},
@@ -9,6 +11,40 @@ const userModel = new mongoose.Schema({
   role:{type:String,enum:["user","admin"], default:"user"},
 
 },{Timestamps:true});
+
+userModel.pre("save",async function(next){
+  const user = this
+  if(!user.isModified('password')) return
+  const salt = randomBytes(21).toString();
+  const hashPass = createHmac("sha256",salt).update(user.password).digest('hex')
+
+  this.salt = salt;
+  this.password = hashPass;
+})
+
+userModel.static("checkToken", async function (email, password) {
+  const user = await this.findOne({ email })
+  if (!user) throw new apiError(404,"No user Found","email is incorrect")
+  const salt = user.salt
+  const hashedPassword = user.password
+  const userPassword = createHmac("sha256", salt).update(password).digest("hex")
+  if (userPassword !== hashedPassword) throw new apiError(404,"Incorrect password","Incorrect pass or email")
+  const token = await createToken(user);
+
+  return token
+})
+
+userModel.method("isPassCorrect", async function (password){
+  const user = await this
+  if (!user) throw new Error("No user Found")
+  const salt = user.salt
+  const hashedPassword = user.password
+  const userPassword = createHmac("sha256", salt).update(password).digest("hex")
+  if (userPassword !== hashedPassword) {
+    return false
+  }
+  return true
+})
 
 const User = mongoose.model("User",userModel);
 
